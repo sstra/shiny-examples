3bb3e7770f16e5b08bb6dcde0bfb3f8f14be6c0a
sstra <40247365+sstra@users.noreply.github.com>
2018-06-28 16:53:38 -0400

    Add files via upload

--- 030-basic-datatable/server.R
+++ 030-basic-datatable/server.R
@@ -4,53 +4,6 @@ library(ggplot2)
 
 function(input, output) {
 
-  # An attempt to input the data as to subsequently download it
-  #datasetInput <- reactive({
-  
-#    switch(input$man, input$man, input$trans, input$cyl)
-    
-    #switch(mpg, "man" = input$man, "trans" = input$trans, "cyl" = input$cyl)
-    # Note: Attempts are reaching point of desperation and attempting code that will
-    # clearly fail.
-    
-#    switch(input$mpg, "man" = mpg$manufacturer, "trans" = mpg$trans,
-#           "cyl" = mpg$cyl
-#    )
-    
-    
-#    switch(input$man, "man" = c("All", unique(as.character(mpg$manufacturer))))
-#    switch(input$trans, "trans" = c("All", unique(as.character(mpg$trans))))
-#    switch(input$cyl, "cyl" =c("All", unique(as.character(mpg$cyl))))
-#    switch(input$mpg, input$man, input$trans, input$cyl)
-    
-#           input$mpg2,
-#           input$man,
-#           input$trans,
-#           input$cyl)
-
-#           "man" = c("All", unique(as.character(mpg$manufacturer))),
-#           "trans" = c("All", unique(as.character(mpg$trans))),
-#           "cyl" = c("All", unique(as.character(mpg$cyl))))
-          
-           #"man" = man,
-           #"trans" = trans,
-           #"cyl" = cyl)
-    
-#    switch(input$mpg,
-#           "man" = switch(input$man, c("All", unique(as.character(mpg$manufacturer)))),
-#           "trans" = switch(input$trans, c("All", unique(as.character(mpg$trans)))),
-#           "cyl" = switch(input$cyl, c("All", unique(as.character(mpg$cyl))))
-#    )
-    
-#    switch(input$fluidRow,
-#           "man" = man,
-#           "trans" = trans,
-#           "cyl" = cyl)
-
-#    switch(input$mpg2, "man" = man, "trans" = trans, "cyl" = cyl)
-#      switch(input$mpg, "man", "trans", "cyl")
-
- # })
   
   currentData <- reactive({
     data <- mpg
